fd09eaab8e23cd2d51e028905797a1c31aa824db
unknown <Makinde-George David>
2025-07-06 22:10:50 +0100

    Fix Vercel build - use esbuild instead of terser

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    minify: 'terser',
+    minify: 'esbuild', 
     rollupOptions: {
       output: {
         manualChunks: {
@@ -20,5 +20,7 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
+  
+  base: './',
 })
